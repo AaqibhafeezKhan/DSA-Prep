a47c21741b9c7f62f8bdbd0839c22ba2dfbb669c
Aaqibhafeez Khan <aaqibhafeezkhan@gmail.com>
2024-10-13 00:19:31 +0530

    Create CheckEvenOdd.java

---
+++ Java/Basics/CheckEvenOdd.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        if (n % 2 == 0) {
+            System.out.println(n + " is even");
+        } else {
+            System.out.println(n + " is odd");
+        }
+    }
+}
