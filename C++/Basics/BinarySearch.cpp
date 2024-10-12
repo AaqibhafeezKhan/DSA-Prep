6f17c6ddb1e80f2acd0b727f29911cf855ea6c79
Aaqibhafeez Khan <aaqibhafeezkhan@gmail.com>
2024-10-13 00:24:29 +0530

    Create BinarySearch.cpp

---
+++ C++/Basics/BinarySearch.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int binary_search(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 3;
+    int result = binary_search(arr, n, target);
+    cout << "Element found at index: " << result << endl;
+    return 0;
+}
