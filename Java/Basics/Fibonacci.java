8586b523715a5d6bcec7eb60bfa175a1188a4fbc
Aaqibhafeez Khan <aaqibhafeezkhan@gmail.com>
2024-10-13 00:18:32 +0530

    Create Fibonacci.java

---
+++ Java/Basics/Fibonacci.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void fibonacci(int n) {
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+    }
+
+    public static void main(String[] args) {
+        fibonacci(10);
+    }
+}
